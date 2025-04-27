29b456a05242eded5791c0b8f206e0c90224b8ca
vikas <vikassony359@gmail.com>
2025-04-27 21:58:09 +0530

    first commit

---
+++ src/main/java/com/LearnGIT1/LearningHGIT/LearningHgitApplication.java
@@ -0,0 +1,13 @@
+package com.LearnGIT1.LearningHGIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningHgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningHgitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearnGIT1/LearningHGIT/LearningHgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnGIT1.LearningHGIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningHgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
